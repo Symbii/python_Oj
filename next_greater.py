b298325e3b756a1e3ca14e87aef023f2224b1a89
Symbii <moon19902009@live.cn>
2017-04-15 13:11:19 +0800

    add next_greater @leetcode

---
+++ next_greater.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+def nextGreaterElement(num1,num2):
+	cache  = {}
+	temp   = []
+	result = []
+	for i in num2:
+		while len(temp) and temp[-1]<i:
+			cache[temp.pop()]=i
+		temp.append(i)
+	for x in num1:
+		if x in cache:
+			result.append(cache[x])
+		else :
+			result.append(-1)
+	return result
+if __name__=='__main__':
+	num1=[2,3,4,6]
+	num2=[1,2,3,7,6,4,5]
+	result = nextGreaterElement(num1,num2)
+	print result	
