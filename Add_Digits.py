db6b6893ebbb16777489cc30c4249c79d209016a
System Administrator <root@liaoxindeMacBook-Pro.local>
2018-04-11 18:22:32 +0800

    up-date

--- Add_Digits.py
+++ Add_Digits.py
@@ -2,6 +2,6 @@
 def add_digits(num):
     return num % 9 if num % 9 else 9 if num else 0
 if __name__ == '__main__':
-    print add_digits(11)
+    print add_digits(9)
 
 
--- reverse_words.py
+++ reverse_words.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import sys
 def reverse_words(s):
-	return '_'.join(map(lambda x:x[::-1], s.split('_')))
+	return '_'.join(map(lambda x:x[::-1], s.split(' ')))
 if __name__ == '__main__':
 	s=sys.argv
 	s.pop(0)
