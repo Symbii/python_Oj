59691e1a69eabd53a9e2ff95ac36916fe4e24a57
Symbii <moon19902009@live.cn>
2018-11-29 00:14:38 +0800

    new func in unque emails

--- Unique_Email_Address.py
+++ Unique_Email_Address.py
@@ -1,24 +1,13 @@
 #!/usr/bin/env python
 
 def Unique_Email_Add(email_list):
-    unique_email = []
+    unique_email = set()
     for str in email_list:
-        domain_index = str.index('@')
-        email_name = ''
-        host_name  = ''
-        for i in range(len(str)):
-            if str[i] == '+':
-                email_name = host_name + (str[domain_index:])
-                break
-            elif str[i] == '.':
-                continue
-            elif str[i] == '@':
-                email_name = host_name + str[domain_index:]
-                break
-            host_name += str[i]
-        if 0 == unique_email.count(email_name):
-            unique_email.append(email_name)
-    return len(unique_email)
+        host, _, domain = str.partition('@')
+        if '+' in host:
+            host = host[:host.index('+')]
+        unique_email.add(host.replace(".","") + '@' + domain)
+    return (unique_email)
 
 if __name__ == "__main__":
     email = ["aaa@163.com","aaa.@163.com","aaa+@163.com","aaa+aa@126.com"]
