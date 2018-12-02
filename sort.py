eb0f28d997b97b83fbdc075ffd1c0d02f1d254f8
Symbii <moon19902009@live.cn>
2018-12-02 23:38:08 +0800

    add merge sort

--- sort.py
+++ sort.py
@@ -36,9 +36,36 @@ def insert_sort(listarry):
         listarry[j] = temp
     return listarry
 
+def merge(left, right):
+    i = 0
+    j = 0
+    newlst = []
+    while i <len(left) and j < len(right):
+        if left[i] > right[j]:
+            newlst.append(right[j])
+            j+=1
+        else:
+            newlst.append(left[i])
+            i+=1
+    if i==len(left):
+        newlst.extend(right[j:])
+    elif j == len(right):
+        newlst.extend(left[i:])
+    return newlst
+def merge_sort(lst):
+    if len(lst) <= 1:
+        return lst
+    mid = len(lst)//2
+    left = merge_sort(lst[:mid])
+    right = merge_sort(lst[mid:])
+    newlst = merge(left, right)
+    return newlst
+
+
 if __name__ == '__main__':
     arry = [-1, 2, 41,-3,-10,-199 ,199,4, 5, 7, 6]
     print(insert_sort(arry))
     print(quick_sort(arry))
     print(select_sort(arry))
     print(bubble_sort(arry))
+    print(merge_sort(arry))
